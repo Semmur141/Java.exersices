ee44df9a794379d1fbde63bdd1e109926584b74d
Semjon Murashov <semjon.murashov@gmail.com>
2023-09-12 16:17:51 +0300

    first commit exercise

---
+++ src/for1.java
@@ -0,0 +1,25 @@
+public class for1 {
+    //public static void main(String[] args) {
+
+        //for (int i = 1; i <= 5; i++) {
+
+            //System.out.print(i + " ");
+
+        //}
+
+   // }
+//}
+
+    //public static void main(String[] args) {
+      //  for (int i = 5; i >= 1; i--) {
+        //    System.out.print(i + " ");
+//        }
+  //  }
+//}
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3 * " + i + " " + "=" + " " + 3 * i);
+        }
+    }
+}
